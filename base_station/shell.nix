fbc972dbc757491015eade492bea1b3f7f2ecd13
Ben Wolsieffer <benwolsieffer@gmail.com>
2021-07-01 15:40:12 -0400

    base_station: add new tools to shell.

--- base_station/shell.nix
+++ base_station/shell.nix
@@ -1,14 +1,17 @@
-{ crossSystem ? null }:
-
-with import <nixpkgs> { inherit crossSystem; };
+{ pkgs ? import <nixpkgs> { } }: with pkgs;
 
 stdenv.mkDerivation {
   name = "base-station-env";
-  nativeBuildInputs = with buildPackages; [
+
+  nativeBuildInputs = [
     rustc
     cargo
     pkgconfig
+    rustfmt
+    clippy
+    crate2nix
   ];
+
   buildInputs = [
     dbus
     openssl
@@ -17,17 +20,12 @@ stdenv.mkDerivation {
   # Doesn't work in debug builds
   hardeningDisable = [ "fortify" ];
 
-  # Set Environment Variables
   RUST_BACKTRACE = 1;
-  
+
   RUST_TOOLCHAIN = buildEnv {
     name = "rust-toolchain";
     paths = with buildPackages; [ rustc cargo ];
   } + "/bin";
-  RUST_SRC_PATH = rustPlatform.rustcSrc;
-
-  PKG_CONFIG_ALLOW_CROSS = 1;
-
-  CARGO_TARGET_ARM_UNKNOWN_LINUX_GNUEABIHF_LINKER = "armv6l-unknown-linux-gnueabihf-cc";
+  RUST_SRC_PATH = rustPlatform.rustLibSrc;
 }
 
